f419747b6798157ca20d5a9ea278e6050375fbfc
Omar Nouhi <nouhiomar@gmail.com>
2019-01-29 20:19:51 +0000

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,10 +10,9 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        {" "}
         <div className="Quiz Question">
           {quizData.quiz_questions[0].instruction_text}
-        </div>{" "}
+        </div>
       </div>
     );
   }
